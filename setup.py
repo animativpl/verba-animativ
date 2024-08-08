9096bab84f4ac977c9470b2f9c91f2d7b0fd6b20
kacpers <kacperek.szczesny@gmail.com>
2024-08-09 00:43:49 +0200

    add paho mqtt to requirements

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "tiktoken==0.6.0",
         "requests==2.31.0",
         "pypdf==4.2.0",
+        "paho-mqtt==2.1.0"
     ],
     extras_require={
         "dev": ["pytest", "wheel", "twine", "black>=23.7.0", "setuptools"],
